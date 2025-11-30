313a1a90cc0f1b4102a3a636ef6b90d09b333b76
shanmukha123307 <anusha.palgot@gmail.com>
2025-11-30 10:08:29 +0530

    Delete right angle triangle.py

--- right angle triangle.py
+++
@@ -1,6 +0,0 @@
-print("half piramid right angle triangle made with (*):")
-n=int(input("enter the number of rows"))
-for i in range(n):
-    for j in range(i+1):
-        print("*",end="")
-    print()
