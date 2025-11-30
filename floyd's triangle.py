91c4978352e63e59e215e5caf91f8906afb0075e
shanmukha123307 <anusha.palgot@gmail.com>
2025-11-30 10:08:04 +0530

    Delete floyd's triangle.py

--- floyd's triangle.py
+++
@@ -1,8 +0,0 @@
-rows=int(input("enter the number of rows:"))
-number=1
-print("floyd's triangle")
-for i in range(1, rows + 1):
-    for j in range(1, i + 1):
-        print(number,end=' ')
-        number += 1
-    print()
